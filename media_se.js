9e8272b339253c88a003bb05d56977da520e3931
andrey123salgueiro <salgueiro.andrey@escola.pr.gov.br>
2026-02-24 09:55:09 -0300

    Add files via upload

---
+++ media_se.js
@@ -0,0 +1,19 @@
+let Aluno="Robson"
+
+let n1=99;
+let n2=100;
+let n3=99;
+let n4=100;
+let media=0;
+let resut="";
+media=(n1+n2+n3+n4)/4;
+if(media>=70){
+    result="aprovado"
+}else{
+    resut="reprvado"
+}
+
+console.log("O aluno "+aluno+" tem a media "+media+"e está:"+resut);
+let media=0;
+let result="";
+
